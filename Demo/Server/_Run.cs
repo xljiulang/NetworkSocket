8f69746060edef140005d17a41e9d536623ec227
xljiulang <366193849@qq.com>
2014-12-05 17:24:16 +0800

    demo修改

--- Demo/Server/_Run.cs
+++ Demo/Server/_Run.cs
@@ -15,37 +15,22 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            Console.Title = "FastServer";
 
-            Console.Title = "综合服务器";
-
-            // silverlight安全策略服务
-            var silverlight = new SilverlightPolicyServer();
-            var owner = TcpTable.Snapshot().FirstOrDefault(item => item.Port == silverlight.Port);
-            if (owner != null)
-            {
-                owner.Kill();
-            }
-            silverlight.StartListen();
-            Console.WriteLine("Sliverlight策略服务启动：" + silverlight.LocalEndPoint);
-
-            // fastserver服务
+            // fastServer服务
             var fastServer = new FastServer();
             fastServer.StartListen(4502);
+            Console.WriteLine("FastServer服务启动：" + fastServer.LocalEndPoint);
 
+
+            // 生成fastServer的代理代码
             var demoPath = Environment.CurrentDirectory;
             while (demoPath.Contains("Server"))
             {
                 demoPath = Path.GetDirectoryName(demoPath);
             }
             fastServer.ToProxyCode().WriteToFile(Path.Combine(demoPath, "ClientApp\\FastServerProxyBase.cs"));
-            Console.WriteLine("FastServer服务启动：" + fastServer.LocalEndPoint);
-
-
-
-            Console.WriteLine();
-            Console.WriteLine("所有服务启动完成....");
-            Console.WriteLine("请运行ClientApp或SilverlightApp或WebSocket.html进行测试...");
-            Console.WriteLine();
+                 
 
             while (true)
             {
