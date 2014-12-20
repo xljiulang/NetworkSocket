31d21028133e2ca1d144c82b12b222634b815d04
xljiulang <366193849@qq.com>
2014-12-20 10:14:19 +0800

    LimitTask

---
+++ Net40/NetworkSocket/Tasks/LimitTask.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkSocket.Tasks
+{
+    /// <summary>
+    /// 表示限制并发数的任务
+    /// </summary>
+    public class LimitTask
+    {
+        /// <summary>
+        /// 任务工厂
+        /// </summary>
+        private TaskFactory taskFactory;
+
+        /// <summary>
+        /// 获取最大并发数
+        /// </summary>
+        public int MaxConcurrencyLevel { get; private set; }
+
+        /// <summary>
+        /// 限制并发数的任务
+        /// </summary>
+        /// <param name="maxConcurrencyLevel">最大并发数</param>
+        public LimitTask(int maxConcurrencyLevel)
+        {
+            this.MaxConcurrencyLevel = maxConcurrencyLevel;
+            var lcts = new LimitTaskScheduler(maxConcurrencyLevel);
+            this.taskFactory = new TaskFactory(lcts);
+        }
+
+        /// <summary>
+        /// 运行并返回一个任务
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public Task Run(Action action)
+        {
+            return this.taskFactory.StartNew(action);
+        }
+
+        /// <summary>
+        /// 运行并返回一个任务
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public Task<T> Run<T>(Func<T> func)
+        {
+            return this.taskFactory.StartNew(func);
+        }
+    }
+}
