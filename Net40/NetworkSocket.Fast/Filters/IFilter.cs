47e8325d621da12a02400b35d76d989910df79bc
xljiulang <366193849@qq.com>
2015-04-01 11:32:35 +0800

    Filter基础接口

---
+++ Net40/NetworkSocket.Fast/Filters/IFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkSocket.Fast.Filters
+{
+    /// <summary>
+    /// 滤过器接口
+    /// </summary>
+    public interface IFilter
+    {
+        /// <summary>
+        /// 获取或设置执行顺序
+        /// 越小最优先
+        /// </summary>
+        int Order { get; set; }
+
+        /// <summary>
+        /// 获取是否允许多个实例存在
+        /// </summary>
+        bool AllowMultiple { get; }
+    }
+}
