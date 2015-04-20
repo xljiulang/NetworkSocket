897ded9a47bd45c9e2e497b2e8be208122ef9f0a
xljiulang <366193849@qq.com>
2015-04-20 09:34:33 +0800

    删除bit类型，直接使用bool值为位的值

--- Net40/NetworkSocket/Bits.cs
+++ Net40/NetworkSocket/Bits.cs
@@ -8,8 +8,9 @@ namespace NetworkSocket
 {
     /// <summary>
     /// 表示字节的位集合
-    /// 与byte类型相互隐式转换
+    /// 值为byte类型
     /// </summary>
+    [Serializable]
     [DebuggerDisplay("{value}")]
     [DebuggerTypeProxy(typeof(DebugView))]
     public struct Bits : IComparable<Bits>
@@ -78,7 +79,7 @@ namespace NetworkSocket
         /// <param name="index">由高到低的位索引(左到右共8位)</param>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         /// <returns></returns>
-        public Bit this[int index]
+        public bool this[int index]
         {
             get
             {
@@ -86,7 +87,7 @@ namespace NetworkSocket
                 {
                     throw new ArgumentOutOfRangeException("index", "index为0到7之间");
                 }
-                return ((this.value & (128 >> index)) > 0) ? 1 : 0;
+                return ((this.value & (128 >> index)) > 0) ? true : false;
             }
             set
             {
@@ -95,7 +96,7 @@ namespace NetworkSocket
                     throw new ArgumentOutOfRangeException("index", "index为0到7之间");
                 }
 
-                if (value == 1)
+                if (value == true)
                 {
                     this.value = (byte)(this.value | (128 >> index));
                 }
@@ -134,40 +135,6 @@ namespace NetworkSocket
         {
             return bits.value;
         }
-
-        /// <summary>
-        /// 从位数据获取字节的位集合
-        /// 索引为从左边最高位到右边最低位顺序
-        /// </summary>
-        /// <param name="bit0">索引0位</param>
-        /// <param name="bit1">索引1位</param>
-        /// <param name="bit2">索引2位</param>
-        /// <param name="bit3">索引3位</param>
-        /// <param name="bit4">索引4位</param>
-        /// <param name="bit5">索引5位</param>
-        /// <param name="bit6">索引6位</param>
-        /// <param name="bit7">索引7位</param>
-        /// <returns></returns>
-        public static Bits From(Bit bit0, Bit bit1, Bit bit2, Bit bit3, Bit bit4, Bit bit5, Bit bit6, Bit bit7)
-        {
-            return Bits.FromBitArray(bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7);
-        }
-
-        /// <summary>
-        /// 从位数组转换得到字节的位集合
-        /// </summary>
-        /// <param name="bitArray">位数组</param>
-        /// <returns></returns>
-        private static Bits FromBitArray(params Bit[] bitArray)
-        {
-            Bits bits = 0;
-            for (var i = 0; i < bitArray.Length; i++)
-            {
-                bits[i] = bitArray[i];
-            }
-            return bits;
-        }
-
 
         /// <summary>
         /// 获取哈希码
@@ -236,14 +203,14 @@ namespace NetworkSocket
             /// 查看的内容
             /// </summary>
             [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
-            public Bit[] Keys
+            public int[] Keys
             {
                 get
                 {
-                    var bitArray = new Bit[8];
+                    var bitArray = new int[8];
                     for (var i = 0; i < bitArray.Length; i++)
                     {
-                        bitArray[i] = this.bits[i];
+                        bitArray[i] = this.bits[i].GetHashCode();
                     }
                     return bitArray;
                 }
