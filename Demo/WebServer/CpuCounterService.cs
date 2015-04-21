a4fa593a0510031177e03b3adc386d462adc2bd3
xljiulang <366193849@qq.com>
2015-04-21 21:54:09 +0800

    遗漏提交

---
+++ Demo/WebServer/CpuCounterService.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using NetworkSocket;
+using NetworkSocket.WebSocket;
+using NetworkSocket.WebSocket.Fast;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocket.Filters;
+
+namespace WebSocket
+{
+    /// <summary>
+    /// Cpu性能检测控制服务
+    /// </summary>   
+    public class CpuCounterService : FastApiService
+    {
+        /// <summary>
+        /// 获取版本号
+        /// </summary>
+        /// <returns></returns>
+        [Api]
+        [LogFilter("获取版本号")]
+        public string GetVersion()
+        {
+            return this.GetType().Assembly.GetName().Version.ToString();
+        }
+
+        /// <summary>
+        /// 订阅/取消Cpu变化通知
+        /// </summary>       
+        /// <returns></returns>
+        [Api]
+        [LogFilter("订阅/取消Cpu变化通知")]
+        public bool SubscribeCpuChangeNotify(bool subscribe)
+        {
+            this.CurrentContext.Session.TagData.Set("NotifyFlag", subscribe);
+            return true;
+        }
+    }
+}
---
+++ Demo/WebServer/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkSocket.WebSocket.Fast;
+using WebSocket.Filters;
+
+namespace WebSocket
+{
+    public class Server : FastWebSocketServer
+    {
+        public Server()
+        {
+            Console.Title = "FastWebSocketServer";
+            GlobalFilters.Add(new ExceptionFilterAttribute());
+            this.BindService<CpuCounterService>();
+            this.StartListen(8282);
+            Console.WriteLine("FastWebSocketServer服务已启动，端口：" + this.LocalEndPoint.Port);
+
+            CpuCounter.CpuTimeChanged += CpuCounter_CpuTimeChanged;
+        }
+
+        private void CpuCounter_CpuTimeChanged(int value)
+        {
+            foreach (var session in this.AllSessions)
+            {
+                if (session.TagData.TryGet<bool>("NotifyFlag", true))
+                {
+                    session.InvokeApi("CpuTimeChanged", new { time = DateTime.Now.ToString("HH:mm:ss"), value });
+                }
+            }
+        }
+
+
+        protected override void OnConnect(FastWebSocketSession session)
+        {
+            Console.Title = "FastWebSocketServer 连接数：" + this.AllSessions.Count();
+        }
+
+        protected override void OnDisconnect(FastWebSocketSession session)
+        {
+            Console.Title = "FastWebSocketServer 连接数：" + this.AllSessions.Count();
+        }
+    }
+}
