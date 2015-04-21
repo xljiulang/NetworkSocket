ce59c5eec9c7564111b9bf13d5c527aafd457cad
xljiulang <366193849@qq.com>
2015-04-21 17:09:33 +0800

    修改HandshakeResponse

--- Net40/NetworkSocket.WebSocket/HandshakeResponse.cs
+++ Net40/NetworkSocket.WebSocket/HandshakeResponse.cs
@@ -37,7 +37,7 @@ namespace NetworkSocket.WebSocket
         /// <returns></returns>
         private string CreateResponseKey()
         {
-            var secKey = this.request.Header["Sec-WebSocket-Key"];
+            var secKey = this.request["Sec-WebSocket-Key"];
             var guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
             var bytes = SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(secKey + guid));
             return Convert.ToBase64String(bytes);
@@ -54,6 +54,7 @@ namespace NetworkSocket.WebSocket
             builder.AppendLine("Upgrade: websocket");
             builder.AppendLine("Connection: Upgrade");
             builder.AppendLine("Sec-WebSocket-Accept: " + this.CreateResponseKey());
+            builder.AppendLine("Server: NetworkSocket.WebSocket");
             builder.AppendLine();
             return Encoding.UTF8.GetBytes(builder.ToString());
         }
