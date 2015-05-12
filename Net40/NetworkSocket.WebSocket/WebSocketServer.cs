f2fc3ddbc4cb97198bfc8661755b37d55d602ef7
xljiulang <366193849@qq.com>
2015-05-12 10:03:05 +0800

    新增WebSocketServer类

---
+++ Net40/NetworkSocket.WebSocket/WebSocketServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkSocket.WebSocket
+{
+    /// <summary>
+    /// WebSocket服务
+    /// 只支持 RFC 6455 协议
+    /// </summary>
+    public class WebSocketServer : WebSocketServerBase<WebSocketSession>
+    {
+        /// <summary>
+        /// 实例化会话对象
+        /// </summary>
+        /// <returns></returns>
+        protected override WebSocketSession OnCreateSession()
+        {
+            return new WebSocketSession();
+        }
+    }
+}
