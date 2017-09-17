11c472dcc957a911d4033e420cc4b033f7a994ec
xljiulang <366193849@qq.com>
2017-09-17 23:45:40 +0800

    过滤器比较

--- NetworkSocket/Core/Internal/DefaultFilterAttributeProvider.cs
+++ NetworkSocket/Core/Internal/DefaultFilterAttributeProvider.cs
@@ -50,11 +50,49 @@ namespace NetworkSocket.Core
         {
             var paramtersFilters = apiAction.GetParametersFilterAttributes();
             var methodFilters = apiAction.GetMethodFilterAttributes();
+            var classFilters = apiAction.GetClassFilterAttributes();
+
+            var allFilters = paramtersFilters
+                .Concat(methodFilters)
+                .Concat(classFilters)
+                .Distinct(new FilterAttributeComparer())
+                .OrderBy(f => f.Order)
+                .ToArray();
+
+            return allFilters;
+        }
+
+        /// <summary>
+        /// 获取Api行为的特性过滤器     
+        /// </summary>
+        /// <param name="apiAction">Api行为</param>
+        /// <returns></returns>
+        private IFilter[] GetActionFiltersNoCachedOld(ApiAction apiAction)
+        {
+            var paramtersFilters = apiAction.GetParametersFilterAttributes();
+            var methodFilters = apiAction.GetMethodFilterAttributes();
             var classFilters = apiAction.GetClassFilterAttributes()
                 .Where(cf => cf.AllowMultiple || methodFilters.Any(mf => mf.TypeId == cf.TypeId) == false);
 
             var allFilters = paramtersFilters.Concat(methodFilters).Concat(classFilters).OrderBy(f => f.Order).ToArray();
             return allFilters;
+        }
+
+        /// <summary>
+        /// FilterAttribute比较器
+        /// </summary>
+        private class FilterAttributeComparer : IEqualityComparer<FilterAttribute>
+        {
+            public bool Equals(FilterAttribute x, FilterAttribute y)
+            {
+                // 如果其中一个不允许重复，返回true将y过滤
+                return x.AllowMultiple == false || y.AllowMultiple == false;
+            }
+
+            public int GetHashCode(FilterAttribute obj)
+            {
+                return obj.TypeId.GetHashCode();
+            }
         }
 
         /// <summary>
